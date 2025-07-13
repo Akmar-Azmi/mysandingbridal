1f6a3dc4d2b555d001cf147740b117b8bea4938f
ShahiraYasmeen <shahirayasmeen@gmail.com>
2025-07-14 01:32:35 +0800

    updatee

--- app/Filament/Pages/Slots.php
+++ app/Filament/Pages/Slots.php
@@ -7,8 +7,25 @@ use Filament\Pages\Page;
 class Slots extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-calendar';
+    protected static ?string $title = 'Available Slots';
+    protected static ?string $navigationLabel = 'Available Slots';
+    protected static ?int $navigationSort = 6;
 
     protected static string $view = 'filament.pages.slots';
 
-    protected static ?int $navigationSort = 6;
+    public $date;
+
+    public function mount(): void
+    {
+        $year = request()->input('year', now()->year);
+        $month = request()->input('month', now()->month);
+        $this->date = Carbon::createFromDate($year, $month, 1);
+    }
+
+    public function getViewData(): array
+    {
+        return [
+            'date' => $this->date,
+        ];
+    }
 }
