b600aa139ba8238fba7cfc6169b8373e2c412e1b
Akmar-Azmi <nurakmarazmi15@gmail.com>
2025-07-14 21:29:58 +0800

    Update render build script for vite

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install PHP dependencies
 composer install --no-dev --optimize-autoloader
 
+# Install Node dependencies & build frontend with Vite
+npm install
+npm run build
+
+# Laravel setup
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
